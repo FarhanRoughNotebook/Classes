cbfdc9b206fe76065b4dbeb38bd9a0a7b276d4e1
FarhanRoughNotebook <ahmedfarhan3a@gmail.com>
2026-07-02 17:53:04 +0530

    Create 2nd_project.c

---
+++ 2nd_project.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void)
+{
+    long int vansh_gupta = 201901145611;
+    long int shivani = 562385785368;
+    long int om = 673696247853;
+    long int jeet = 572578357835;
+    long int farhan = 982645890535;
+    
+    printf("------------------------\n");
+    printf("|  NAME |  NUMBER      |\n");
+    printf("------------------------\n");
+    printf("|Om     |  %li|\n", om);
+    printf("|Jeet   |  %li|\n",jeet);
+    printf("|Vansh  |  %li|\n",vansh_gupta);
+    printf("|Shivani|  %li|\n",shivani);
+    printf("|Farhan |  %li|\n",farhan);
+   printf("------------------------\n");
+}
